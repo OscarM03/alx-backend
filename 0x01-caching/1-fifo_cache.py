d9921cc3956ccef877d440b1d6decd14df093dcd
OscarM03 <oscarmutwiri03@gmail.com>
2024-06-26 16:02:54 +0300

    FIFO caching system that inherits from BaseCaching

---
+++ 0x01-caching/1-fifo_cache.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""FIFO caching"""
+
+from base_caching import BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    """caching system that inherits from BaseCaching"""
+    def __init__(self):
+        super().__init__()
+
+    def put(self, key, item):
+        """Adds an item in the cache"""
+        if key is None or item is None:
+            return
+        if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+            key1 = next(iter(self.cache_data))
+            print("Discard: {}".format(key1))
+            del self.cache_data[key1]
+        self.cache_data[key] = item
+
+    def get(self, key):
+        """Get an item from the cache"""
+        if key is None:
+            return None
+        return self.cache_data.get(key)
