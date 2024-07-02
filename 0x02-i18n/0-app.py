44a866880a7e0d5b29a02a56dc8088f761f67777
OscarM03 <oscarmutwiri03@gmail.com>
2024-07-02 12:23:07 +0300

    Debugging pycodestyle

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,11 +1,17 @@
+#!/usr/bin/env python3
+"""Basic flask app"""
+
 from flask import Flask, render_template
+
 
 app = Flask(__name__)
 
-@app.route('/')
+
+@app.route('/', strict_slashes=False)
 def index():
     """index route"""
     return render_template('0-index.html')
 
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
