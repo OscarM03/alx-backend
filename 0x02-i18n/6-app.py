1fae33f55f7ba5b5ba3a899a5a49e7a169d00ebd
OscarM03 <oscarmutwiri03@gmail.com>
2024-07-02 21:17:24 +0300

    Correcting the html file

--- 0x02-i18n/6-app.py
+++ 0x02-i18n/6-app.py
@@ -57,7 +57,7 @@ def get_locale():
 @app.route('/', strict_slashes=False)
 def index():
     """index route"""
-    return render_template('5-index.html')
+    return render_template('6-index.html')
 
 
 if __name__ == '__main__':
