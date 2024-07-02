90ba855cc2c7f2768fb65547f14591522623fdb4
OscarM03 <oscarmutwiri03@gmail.com>
2024-07-02 21:17:45 +0300

    Correcting the html file

--- 0x02-i18n/7-app.py
+++ 0x02-i18n/7-app.py
@@ -77,7 +77,7 @@ def get_timezone():
 @app.route('/', strict_slashes=False)
 def index():
     """index route"""
-    return render_template('5-index.html')
+    return render_template('7-index.html')
 
 
 if __name__ == '__main__':
